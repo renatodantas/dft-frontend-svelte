43669b1cf03d448f1a390fe65c947bff26f7232d
Renato Dantas <rexdantas@gmail.com>
2021-02-02 18:36:31 -0300

    Grouping routes into one component

---
+++ src/Routes.svelte
@@ -0,0 +1,23 @@
+<script lang="typescript">
+  import { Route } from "tinro";
+  import Lazy from "./utils/Lazy.svelte";
+</script>
+
+<Route path="/">
+  <Lazy component={import("./routes/Home.svelte")} />
+</Route>
+
+<!-- Tipificação/* -->
+<Route path="/tipificacao">
+  <Lazy component={import("./routes/tipificacao/Tipificacao.svelte")} />
+</Route>
+<Route path="/tipificacao/categorias">
+  <Lazy
+    component={import("./routes/tipificacao/categorias/CategoriaList.svelte")}
+  />
+</Route>
+<Route path="/tipificacao/categorias/:id">
+  <Lazy
+    component={import("./routes/tipificacao/categorias/CategoriaEdit.svelte")}
+  />
+</Route>
